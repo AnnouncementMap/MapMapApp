6b4ab691cf0427bc85a9723ad7b2fa439806a871
hinesd <hinesdillon01@gmail.com>
2024-05-13 20:38:57 -0400

    adding migrations

--- datamap/urls.py
+++ datamap/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from MapMapApp import views
+from datamap import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test_response/', views.test_response),
